d5ea533e9474e5c73f98e310563f363f08dc8d71
Maryna01~ <maryna.naidych@gmail.com>
2023-04-04 13:09:28 +0300

    made adaptive images

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 let burger = document.querySelector(".burger");
 let burgerMenu = document.querySelector(".nav-list");
-let burgerItem = document.querySelector(".nav-item");
 
 burger.addEventListener('click', function(){
   burger.classList.toggle('active');
   burgerMenu.classList.toggle("burger-menu");
-  burgerItem.classList.toggle("burger-item");
 })
